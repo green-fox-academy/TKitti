af7b7f5bc2a7f8d65d15b46f4600f01bcc60379d
Kitti <kittitoth8@gmail.com>
2019-01-17 15:39:57 +0100

    define-basic-info exercise

---
+++ week-01/day-04/define-basic-info.ts
@@ -0,0 +1,11 @@
+"use strict";
+
+let myName:string = "Kitti";
+let myAge: number = 27;
+let myHeight: number = 1.60;
+let isMarried: boolean = false;
+
+console.log(myName);
+console.log(myAge);
+console.log(myHeight);
+console.log(isMarried);
