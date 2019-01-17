eafb3f2cc89f7f0580e45e5bf8ce812b6b1e35d0
Kitti <kittitoth8@gmail.com>
2019-01-17 15:34:47 +0100

    bmi exercise

---
+++ week-01/day-04/bmi.ts
@@ -0,0 +1,8 @@
+"use strict";
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+console.log(Math.round(massInKg / heightInM**2));
+//heightInM at power 2 , (1,78*1,78)
+//Math.round rounds the result to decimal number
