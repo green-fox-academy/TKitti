106bed6270749ac4ab8bd3c11f0eec25e97e2d82
Kitti <kittitoth8@gmail.com>
2019-01-17 16:27:12 +0100

    cuboid exercise

---
+++ week-01/day-04/cuboid.ts
@@ -0,0 +1,8 @@
+"use strict";
+
+let surfaceArea: number = 600;
+let volume: number = 1000;
+let thirdSide: number;
+
+console.log("Surface Area: " + surfaceArea);
+console.log("Volume: " + volume);
