1e77efcd5bcc97afec3d236eabf49d6fe001b83c
Kitti <kittitoth8@gmail.com>
2019-01-17 14:56:13 +0100

    two-numbers exercise

---
+++ week-01/day-04/two-numbers.ts
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log(13+22);
+console.log(22-13);
+console.log(22*13);
+console.log(Number((22/13).toFixed(1)));
+//using toFixed method returns a string, therefore the result is converted
+//to a number by Number() method
+
+console.log(22%13);
