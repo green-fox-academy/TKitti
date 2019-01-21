bee9c974c03755bce563cff218e7135bf8bb81ea
Kitti <kittitoth8@gmail.com>
2019-01-21 15:24:57 +0100

    print-all exercise

---
+++ week-02/day-06/print-all.ts
@@ -0,0 +1,17 @@
+"use strict";
+export{}
+
+
+let integers = [4, 5, 6, 7];
+
+/*
+for (let counter = 0; counter < integers.length; counter ++) {
+    console.log(integers[counter]);
+}
+*/
+
+//OR:
+
+integers.forEach(function(number) {
+    console.log(number);
+});
