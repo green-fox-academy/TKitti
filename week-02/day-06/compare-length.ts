f4f1d9ade68c261a878d1d861f7fb45502d570e6
Kitti <kittitoth8@gmail.com>
2019-01-21 14:51:02 +0100

    compare-length exercise

---
+++ week-02/day-06/compare-length.ts
@@ -0,0 +1,12 @@
+"use strict";
+export{}
+
+
+let firstList = [1, 2, 3];
+let secondList = [4, 5];
+
+if (secondList.length > firstList.length) {
+    console.log(true);
+} else {
+    console.log(false);
+}
