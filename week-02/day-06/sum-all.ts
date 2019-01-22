b8b7f573f8e2554e1b7962e96ef70e9ad0b9a8f2
Kitti <kittitoth8@gmail.com>
2019-01-22 10:11:46 +0100

    modifying sum-all exercise

--- week-02/day-06/sum-all.ts
+++ week-02/day-06/sum-all.ts
@@ -2,10 +2,20 @@
 export{}
 
 let ai: number [] = [3, 4, 5, 6, 7];
+
+/*
 let sum = 0;
 
 ai.forEach(function (arrayNumber) {
     sum = sum + arrayNumber;
 });
+
+console.log(sum);
+*/
+
+
+let sum = ai.reduce(function(accumulator, currentValue) {
+    return accumulator + currentValue;
+}, 0);
 
 console.log(sum);
