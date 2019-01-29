07a932c72587a603695acf5b579053abbb1d9c68
Kitti <kittitoth8@gmail.com>
2019-01-29 19:38:31 +0100

    5-trees homework

---
+++ week-03/day-13/5-trees-homework.ts
@@ -0,0 +1,9 @@
+'use strict'
+export { }
+
+
+let tree1: any[] = ['oak', 'green', 25, 'male'];
+let tree2: any[] = ['palm', 'green', 20, 'female'];
+let tree3: any[] = ['birch', 'brown', 10, 'female'];
+let tree4: any[] = ['maple', 'brown', 30, 'female'];
+let tree5: any[] = ['pine', 'green', 38, 'male'];
