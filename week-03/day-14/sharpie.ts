9758c8300eb07f464e013074d5c725361f258c57
Kitti <kittitoth8@gmail.com>
2019-01-31 16:36:04 +0100

    sharpie-set exercise

--- week-03/day-14/sharpie.ts
+++ week-03/day-14/sharpie.ts
@@ -1,5 +1,4 @@
 'use strict'
-export { }
 
 
 class Sharpie {
@@ -7,14 +6,21 @@ class Sharpie {
   width: number;
   inkAmount: number = 100.00;
 
-  constructor(colorInput: string, widthInput: number) {
+  constructor(colorInput: string, widthInput: number, inputInkAmount: number) {
     this.color = colorInput;
     this.width = Number(widthInput.toFixed(2));
+    this.inkAmount = inputInkAmount;
   }
 
   use() {
+    if (this.inkAmount > 0) {
     this.inkAmount = this.inkAmount - 1;
+    } else {
+      this.inkAmount = 0;
+    }
   }
 }
 
-let sharp1 = new Sharpie("grey", 2.8);
+let sharp1 = new Sharpie("grey", 2.8, 20);
+
+export { Sharpie };
