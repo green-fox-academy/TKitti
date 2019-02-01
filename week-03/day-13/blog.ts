ec58e91731f05845bda3f1b234ca5a7fc8f03c6e
Kitti <kittitoth8@gmail.com>
2019-02-01 14:12:25 +0100

    blog exercise

--- week-03/day-13/blog.ts
+++ week-03/day-13/blog.ts
@@ -1,12 +1,26 @@
 'use strict'
 
+import { BlogPost } from './blogpost';
 
-export class BlogPost {
-  authorName: string;
-  title: string;
-  text: string;
-  publicationDate: string;
+
+class Blog {
+  manyPosts: BlogPost[] = [];
+
+  add(newPost: BlogPost) {
+    this.manyPosts.push(newPost);
+  }
+
+  delete(indexOfPost: number) {
+    let newArrayAfterDelete = this.manyPosts.splice(indexOfPost, 1);
+    return newArrayAfterDelete;
+  }
+
+  update(indexOfPost: number, newpost: BlogPost) {
+    let newArrayAfterUpdate = this.manyPosts.splice(indexOfPost, 1, newpost);
+    return newArrayAfterUpdate;
+  }
 }
+
 
 let post1 = new BlogPost();
 post1.authorName = 'John Doe';
@@ -25,3 +39,24 @@ post3.authorName = 'William Turton';
 post3.title = 'One Engineer Is Trying to Get IBM to Reckon With Trump';
 post3.text = 'Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.';
 post3.publicationDate = '2017.03.28.';
+
+
+let myBlog = new Blog;
+
+myBlog.manyPosts.push(post1);
+myBlog.manyPosts.push(post2);
+myBlog.manyPosts.push(post3);
+
+
+let post4 = new BlogPost;
+post4.authorName = 'Sally';
+post4.title = 'Hello';
+post4.text = 'This is a sample blogpost.';
+post4.publicationDate = '2019.02.01';
+myBlog.add(post4);
+
+
+myBlog.delete(1);
+myBlog.update(2, post2);
+
+console.log(myBlog);
