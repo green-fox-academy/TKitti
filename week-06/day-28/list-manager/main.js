a51d9b03d79042639563e8ea63b12f4ac0492200
Kitti <kittitoth8@gmail.com>
2019-02-27 16:19:50 +0100

    implementing functionality to the delete button

--- week-06/day-28/list-manager/main.js
+++ week-06/day-28/list-manager/main.js
@@ -52,3 +52,68 @@ rightList.setAttribute('style',
 container.appendChild(rightList);
 
 
+//setting default highlight to the first element in the list
+leftList.children[0].style.backgroundColor = 'grey';
+
+
+
+//clicking on the DOWN button
+let down = middleList.children[3];
+down.onclick = function (event) {
+  //saving the left list ul into a variable on the basis of the current event
+  let leftListWithEvent = event.currentTarget.parentElement.previousSibling;
+
+  let indexOfHighlightedElement = 0;
+  
+  for (let i = 0; i < leftListWithEvent.childElementCount; i++) {
+    if (leftListWithEvent.children[i].style.backgroundColor === 'grey') {
+      indexOfHighlightedElement = i;
+    }
+  }
+
+  //we need if statement, to stop the moving of the highlighting when the highlight is on the last item
+  if (indexOfHighlightedElement !== leftListWithEvent.childElementCount - 1) {
+    leftListWithEvent.children[indexOfHighlightedElement].style.backgroundColor = 'white';
+    leftListWithEvent.children[indexOfHighlightedElement + 1].style.backgroundColor = 'grey';
+  }
+};
+
+
+//clicking on the UP button
+let up = middleList.children[0];
+up.onclick = function (event) {
+  let leftListWithEvent = event.currentTarget.parentElement.previousSibling;
+
+  let indexOfHighlightedElement = 0;
+  
+  for (let i = 0; i < leftListWithEvent.childElementCount; i++) {
+    if (leftListWithEvent.children[i].style.backgroundColor === 'grey') {
+      indexOfHighlightedElement = i;
+    }
+  }
+
+  //we need if statement, to stop the moving of the highlighting when the highlight is on the last item
+  if (indexOfHighlightedElement !== 0) {
+    leftListWithEvent.children[indexOfHighlightedElement].style.backgroundColor = 'white';
+    leftListWithEvent.children[indexOfHighlightedElement - 1].style.backgroundColor = 'grey';
+  }
+};
+
+
+
+//clicking on the X button
+let deleteButton = middleList.children[2];
+deleteButton.onclick = function (event) {
+  let leftListWithEvent = event.currentTarget.parentElement.previousSibling;
+
+  let indexOfHighlightedElement = 0;
+  
+  for (let i = 0; i < leftListWithEvent.childElementCount; i++) {
+    if (leftListWithEvent.children[i].style.backgroundColor === 'grey') {
+      indexOfHighlightedElement = i;
+    }
+  }
+
+  leftListWithEvent.children[indexOfHighlightedElement].remove();
+  leftListWithEvent.children[0].style.backgroundColor = 'grey';
+}
