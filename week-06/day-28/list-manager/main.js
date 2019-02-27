778e8027cb3835266c07c65a9b762c443e9dd6f4
Kitti <kittitoth8@gmail.com>
2019-02-27 14:44:12 +0100

    creating middle box with buttons

--- week-06/day-28/list-manager/main.js
+++ week-06/day-28/list-manager/main.js
@@ -1,13 +1,14 @@
 let body = document.getElementsByTagName('body')[0];
 let script = document.getElementsByTagName('script')[0];
 let container = document.createElement('div');
+body.insertBefore(container, script);
+
 
 let leftList = document.createElement('ul');
 leftList.setAttribute('style', 
-    'border: 1px solid black; width: 120px; min-height: 92px; padding: 0px;');
+'border: 1px solid black; width: 120px; min-height: 92px; padding: 0px;');
 
 container.appendChild(leftList);
-body.insertBefore(container, script);
 
 
 let items = ['bread', 'milk', 'orange', 'tomato'];
@@ -16,6 +17,24 @@ for (let i = 0; i < items.length; i++) {
   let listElement = document.createElement('li');
   listElement.textContent = items[i];
   listElement.setAttribute('style', 'list-style-type: none;');
-  console.log(items[i]);
   leftList.appendChild(listElement);
+}
+
+
+let middleList = document.createElement('ul');
+middleList.setAttribute('style', 
+'width: 120px; min-height: 92px; padding: 0px; text-align: center;');
+container.appendChild(middleList);
+
+let buttons = ['Up', '>', 'X', 'Down'];
+
+for (let i = 0; i < buttons.length; i++) {
+  let listElement = document.createElement('li');
+  let buttonElement = document.createElement('button');
+  buttonElement.textContent = buttons[i];
+  listElement.setAttribute('style', 'list-style-type: none;');
+  buttonElement.setAttribute('style', 
+  'width: 120px;');
+  listElement.appendChild(buttonElement);
+  middleList.appendChild(listElement);
 }
