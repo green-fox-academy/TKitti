2cf357692d09fa0b55382cac319fc546a553fdbe
Kitti <kittitoth8@gmail.com>
2019-04-04 11:10:54 +0200

    adding error handling to search city funcionality

--- project-phase/week-0/weather-app/src/app/app.component.ts
+++ project-phase/week-0/weather-app/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { WeatherInfoService } from './cities/weather-info.service';
 import { City } from './city';
 import { ResponseInfo } from './responseInterface';
+import { error } from '@angular/compiler/src/util';
+import { HttpErrorResponse } from '@angular/common/http';
 
 
 @Component({
@@ -13,6 +15,8 @@ export class AppComponent {
   title = 'weather-app';
 
   firstCity = new City('', '', 0, '');
+  error;
+  errorMessageForUser: string = '';
 
   cities: City[] = [
     this.firstCity
@@ -24,20 +28,30 @@ export class AppComponent {
 
   ngOnInit() {
     this.infoService.getRandomCitiesWeatherInfo().subscribe((response: ResponseInfo[]) => {
-      console.log(response.list[0].main.temp);
-      this.cities = response.list.map(oneCity => { return new City(oneCity.name, oneCity.sys.country, Math.round((oneCity.main.temp)), `../../assets/${oneCity.weather[0].main}.png`)});
-    });
+      this.cities = response.list.map(oneCity => { return new City(oneCity.name, oneCity.sys.country, Math.round((oneCity.main.temp)), `../../assets/${oneCity.weather[0].main}.png`) });
+    }, error => this.error = error);
   }
 
   search() {
-    this.infoService.getWeatherInfo(this.inputValue).subscribe((response: ResponseInfo) => {
-      this.cities = [
-        this.firstCity
-      ];
-      this.cities[0].cityName = response.name;
-      this.cities[0].countryName = response.sys.country;
-      this.cities[0].celsius = Math.round((response.main.temp)) - 273;
-      this.cities[0].imgSource = `../../assets/${response.weather[0].main}.png`;
-    });
+    this.infoService.getWeatherInfo(this.inputValue).subscribe(
+      (response: ResponseInfo) => {
+          this.cities = [
+            this.firstCity
+          ];
+          this.cities[0].cityName = response.name;
+          this.cities[0].countryName = response.sys.country;
+          this.cities[0].celsius = Math.round((response.main.temp)) - 273;
+          this.cities[0].imgSource = `../../assets/${response.weather[0].main}.png`;
+
+          //clear input field after search
+          this.inputValue = '';
+          //delete error message if it exists
+          this.errorMessageForUser = '';
+      }, error => { 
+        this.error = error;
+        if (this.error.status === 400 || this.error.status === 404) {
+          this.errorMessageForUser = 'City was not found';
+      }}
+    );
   }
 }
--- project-phase/week-0/weather-app/src/app/cities/weather-info.service.ts
+++ project-phase/week-0/weather-app/src/app/cities/weather-info.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable, ErrorHandler } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +10,10 @@ export class WeatherInfoService {
   constructor(private http: HttpClient) { }
 
   getWeatherInfo (inputCity) {
-    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&APPID=e32c7dbda17032dcb301ab0e8593816f`);
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=e32c7dbda17032dcb301ab0e8593816f`)
   }
 
   getRandomCitiesWeatherInfo () {
-    return this.http.get(`http://api.openweathermap.org/data/2.5/group?id=524901,703448,2643743&units=metric&APPID=e32c7dbda17032dcb301ab0e8593816f`);
+    return this.http.get(`http://api.openweathermap.org/data/2.5/group?id=524901,703448,2643743&units=metric&appid=e32c7dbda17032dcb301ab0e8593816f`)
   }
 }
